6ca50d7b91616b8f25194f2cf9275747420bc074
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2011-04-11 16:05:39 +0900

    write mock and fix to works

--- examples/hive_methods.js
+++ examples/hive_methods.js
@@ -20,15 +20,9 @@ connection.addListener("connect", function() {
         console.log("queryplan queryAttributes:", data.queries[0].queryAttributes);
         console.log("queryplan stageGraph:", data.queries[0].stageGraph);
         console.log("queryplan stageGraph adjacencyList children:", data.queries[0].stageGraph.adjacencyList[0].children);
-        console.log("queryplan stageGraph adjacencyList children:", data.queries[0].stageGraph.adjacencyList[1].children);
         console.log("queryplan stageList:", data.queries[0].stageList);
         console.log("queryplan stageList taskList:", data.queries[0].stageList[0].taskList[0]);
         console.log("queryplan stageList taskList operatorGraph adjacencyList:", data.queries[0].stageList[0].taskList[0].operatorGraph.adjacencyList);
-        console.log("queryplan stageList taskList:", data.queries[0].stageList[0].taskList[1]);
-        console.log("queryplan stageList taskList:", data.queries[0].stageList[1].taskList[0]);
-        console.log("queryplan stageList taskList:", data.queries[0].stageList[1].taskList[1]);
-        console.log("queryplan stageList taskList:", data.queries[0].stageList[2].taskList[0]);
-        console.log("queryplan stageList taskList:", data.queries[0].stageList[2].taskList[1]);
 
         client.getSchema(function(err, data){
           console.log("getSchema:", data);
--- hive_server_mock.js
+++ hive_server_mock.js
@@ -2,24 +2,110 @@ var thrift = require('thrift'),
     ttransport = require('thrift/transport');
 var ThriftHive = require('gen-nodejs/ThriftHive');
 
-var mocks = 
-};
+var mock = require('ThriftHiveMock');
+/*
+ * ThriftHive server mock with dummy response,
+ *  cannot handle concurrent connections...
+ *  cannot send response with exceptions...
+ */
 
 var query = undefined;
-var query_info = {};
+var query_plan = undefined;
+var query_schema = undefined;
 var query_result = [];
 
-var execute = function(query, success){
-  /* hogehoge */
-  success();
+var waited_queries = [];
+
+var init_status = function(){
+  query_plan = query_schema = query = undefined;
+  query_result = [];
 };
-var getClusterStatus = function(success){};
-var fetchOne = function(success){ success(returned_result); };
-var fetchN = function(rows, success){ success(returned_results_rows); };
-var fetchAll = function(success){ success(returned_results_all); };
-var getSchema = function(success){};
-var getThriftSchema = function(success){};
-var getQueryPlan = function(success){};
+
+setInterval(function(){
+  if (query != undefined || waited_queries.length < 1) {
+    return;
+  }
+  query_plan = mock.query_plan(waited_queries[0]);
+  query_schema = mock.schema(waited_queries[0]);
+  query_result = mock.result(waited_queries[0]);
+  query = waited_queries.shift();
+}, 250);
+
+var execute = function(queued_query, success){
+  waited_queries.push(queued_query);
+
+  var timer = setInterval(function(){
+    if (query != queued_query) {
+      return;
+    };
+    clearInterval(timer);
+    success();
+  }, 500);
+};
+
+var getClusterStatus = function(success){
+  success(mock.cluster_status());
+};
+
+var fetchOne = function(success){
+  if (query == undefined) {
+    success('');
+    return;
+  }
+  var val = '';
+  if (query_result.length > 0) {
+    val = query_result.length.shift();
+  }
+  if (query_result.length == 0) {
+    init_status();
+  }
+  success(val);
+};
+
+var fetchN = function(rows, success){
+  if (query == undefined) {
+    success(['']);
+    return;
+  }
+  var val = [];
+  for (var i = 0; i < rows; i++) {
+    val.push(query_result.shift());
+    if (query_result.length < 1) {
+      init_status();
+      break;
+    }
+  }
+  success(val);
+};
+
+var fetchAll = function(success){
+  if (query == undefined) {
+    success(['']);
+    return;
+  }
+  var val = query_result;
+  init_status();
+  success(val);
+};
+
+var getSchema = function(success){
+  if (query_schema == undefined) {
+    success(mock.schema());
+    return;
+  }
+  success(query_schema);
+};
+
+var getThriftSchema = function(success){
+  getSchema(success);
+};
+
+var getQueryPlan = function(success){
+  if (query_plan == undefined) {
+    success(mock.query_plan());
+  }
+  success(query_plan);
+};
 
 var server_mock = thrift.createServer(ThriftHive, {
   getClusterStatus: getClusterStatus,
