fa0a5e1633a728914834b295b3af470bac14f23e
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2011-04-19 18:20:15 +0900

    add tests

--- test/t_query.js
+++ test/t_query.js
@@ -1,5 +1,4 @@
 var testCase = require('nodeunit').testCase;
-
 var query = require('shib/query');
 
 module.exports = testCase({
@@ -8,13 +7,28 @@ module.exports = testCase({
     this.foo = 'bar';
     callback();
   },
-  tearDown: function (callback) {
-    // clean up
-    callback();
-  },
    */
   test1: function (test) {
     test.equals('bar', 'bar');
     test.done();
+  },
+  removeNewLines: function(test) {
+    test.equals('hoge', query.removeNewLines('hoge'));
+    test.equals("hoge", query.removeNewLines("hoge\n"));
+    test.equals("hoge", query.removeNewLines("ho\nge"));
+    test.equals("hoge", query.removeNewLines("\nhoge"));
+    test.equals("hoge", query.removeNewLines("\nhoge\n"));
+    test.equals("hoge", query.removeNewLines("\nho\n\nge\n"));
+    test.done();
+  },
+  checkQueryString: function(test) {
+    test.done();
+  },
+  generateQueryId: function(test) {
+    test.done();
+  },
+  tearDown: function (callback) {
+    // clean up
+    callback();
   }
 });
