364204b6db960c32f9752a94240cc690569fa3e5
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-04-20 19:25:05 +0900

    add tests

--- test/t_query.js
+++ test/t_query.js
@@ -1,5 +1,7 @@
 var testCase = require('nodeunit').testCase;
-var query = require('shib/query');
+var query = require('shib/query'),
+    Query = query.Query;
+
 
 module.exports = testCase({
   /*
@@ -22,9 +24,25 @@ module.exports = testCase({
     test.done();
   },
   checkQueryString: function(test) {
+    test.throws(function(){Query.checkQueryString('');});
+    test.throws(function(){Query.checkQueryString('select count(*)');});
+    test.throws(function(){Query.checkQueryString('show tables');});
+    test.throws(function(){Query.checkQueryString('describe hoge_table');});
+    test.throws(function(){Query.checkQueryString('create table hoge (col1 as string, col2 as bigint)');});
+    test.throws(function(){Query.checkQueryString('drop table hoge');});
+    test.throws(function(){Query.checkQueryString('alter table hoge (col1 as smallint)');});
+    test.throws(function(){Query.checkQueryString("insert overwrite into hoge\nselect col1,col2 from hoge where moge='pos'");});
+    test.ok(Query.checkQueryString("select field1,field2,count(*) as cnt from hoge_table where yyyymmdd=today()"));
+    test.ok(Query.checkQueryString(" select field1,field2,count(*) as cnt from hoge_table where yyyymmdd=today()"));
+    test.throws(function(){
+      Query.checkQueryString("select field1,field2,count(*) as cnt from hoge_table where yyyymmdd=today(); drop table hoge_table");
+    });
     test.done();
   },
   generateQueryId: function(test) {
+    test.equals(Query.generateQueryId('select * from hoge where id=111', []), 'cec9ab9d980c1b3ed582471dc79eb65b');
+    test.equals(Query.generateQueryId('select * from hoge where id=111', ['keyword1']), 'ab54323b5bddaad03be698464e9a60a9');
+    test.equals(Query.generateQueryId('select * from hoge where id=111', ['keyword1', 'keyword2']), 'aacb8f731fd4850472c3c4963cbb085f');
     test.done();
   },
   tearDown: function (callback) {
