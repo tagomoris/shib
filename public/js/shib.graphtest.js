b349f5eb864f5bd65101b9d10924ac911f97bae7
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-06-09 16:34:35 +0900

    fix layout draft and pains

--- public/js/shib.graphtest.js
+++ public/js/shib.graphtest.js
@@ -1,4 +1,5 @@
-function render_google_chart_api(chart_div_id, title, columns, data){
+function render_google_chart_api(chart_div_id, title, columns, data, options){
+  var default_options = {width: 750, height: 450};
   var datatable = new google.visualization.DataTable();
   var types = [];
   columns.forEach(function(c){
@@ -16,5 +17,12 @@ function render_google_chart_api(chart_div_id, title, columns, data){
   }
 
   var chart = new google.visualization.LineChart(document.getElementById(chart_div_id));
-  chart.draw(datatable, {width: 800, height: 480, title:title});
+  if (! options)
+    options = {};
+  options.title = title;
+  if (! options.width)
+    options.width = default_options.width;
+  if (! options.height)
+    options.height = default_options.height;
+  chart.draw(datatable, options);
 };
