188b54ea5dffcb92be416981a2a641ef5883c6ce
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-06-20 19:46:20 +0900

    writing interactions...

--- public/js/shib.js
+++ public/js/shib.js
@@ -6,7 +6,7 @@ var shibselectedquery = null;
 var shibselectedquery_dom = null;
 
 var shib_NOTIFICATION_CHECK_INTERVAL = 100;
-var shib_NOTIFICATION_LAST_DURATION_COUNT = 200;
+var shib_NOTIFICATION_DEFAULT_DURATION_COUNT = 200;
 
 $(function(){
   $.getJSON('/summary_bulk', function(data){
@@ -69,7 +69,7 @@ $(function(){
   $('#test_status_change_running').click(function(event){shib_test_status_change("running");});
   $('#test_status_change_executed').click(function(event){shib_test_status_change("executed");});
   $('#test_status_change_error').click(function(event){shib_test_status_change("error");});
-  $('#test_status_change_rerunning').click(function(event){shib_test_status_change("rerunning");});
+  $('#test_status_change_rerunning').click(function(event){shib_test_status_change("re-running");});
   $('#test_show_notice_bar').click(function(event){$('#infoarea').toggle();});
   $('#test_show_error_bar').click(function(event){$('#errorarea').toggle();});
   /* **** **** */
@@ -85,21 +85,29 @@ $(function(){
   });
 });
 
+function last_result(query) {
+  var obj = null;
+  if (query && query.results && query.results.length > 0 && query.results[query.results.length - 1])
+    if ((obj = shibdata.result_cache[query.results[query.results.length - 1].resultid]) !== null)
+      return obj;
+  return null;
+}
+
 var shib_current_notification = null;
 var shib_current_notification_counter = 0;
 function show_notification(){
   if (shib_current_notification === null && shibnotifications.length == 0)
     return;
   if (shib_current_notification !== null && shibnotifications.length == 0){
-    shib_current_notification_counter += 1;
-    if (shib_current_notification_counter > shib_NOTIFICATION_LAST_DURATION_COUNT) {
+    shib_current_notification_counter -= 1;
+    if (shib_current_notification_counter < 1) {
       shib_current_notification.fadeOut(100);
       shib_current_notification_counter = 0;
     }
     return; 
   }
   var next = shibnotifications.shift();
-  shib_current_notification_counter = 0;
+  shib_current_notification_counter = next.duration || shib_NOTIFICATION_DEFAULT_DURATION_COUNT;
   if (shib_current_notification) {
     shib_current_notification.fadeOut(100, function(){
       shib_current_notification = update_notification(next.type, next.title, next.message);
@@ -123,12 +131,12 @@ function update_notification(type, title, message){
   return $('#errorarea');
 };
 
-function show_info(title, message){
-  shibnotifications.push({type:'info', title:title, message:message});
+function show_info(title, message, duration){
+  shibnotifications.push({type:'info', title:title, message:message, duration:duration});
 };
 
-function show_error(title, message){
-  shibnotifications.push({type:'error', title:title, message:message});
+function show_error(title, message, duration){
+  shibnotifications.push({type:'error', title:title, message:message, duration:duration});
 };
 
 
@@ -146,17 +154,16 @@ function create_queryitem_object(queryid, id_prefix){
   var query = shibdata.query_cache[queryid];
   if (! query)
     return '';
-  var lastresult = (query.results && query.results.length > 0 && query.results[query.results.length - 1]) || null;
-  var lastresultobj = (lastresult && shibdata.result_cache[lastresult.resultid]) || null;
+  var lastresult = last_result(query);
   var executed_at = (lastresult && lastresult.executed_at) || '-';
   var keyword_primary = (query.keywords && query.keywords.length > 0 && query.keywords[0]) || '-';
   return {
     QueryId: (id_prefix || '') + query.queryid,
     Information: executed_at + ', ' + keyword_primary,
     Statement: query.querystring,
-    Status: (lastresultobj && lastresultobj.state) || 'running',
-    Etc: (lastresultobj && lastresultobj.bytes && lastresultobj.lines &&
-          (lastresultobj.bytes + ' bytes, ' + lastresultobj.lines + ' lines')) || ''
+    Status: (lastresult && lastresult.state) || 'running',
+    Etc: (lastresult && lastresult.bytes && lastresult.lines &&
+          (lastresult.bytes + ' bytes, ' + lastresult.lines + ' lines')) || ''
   };
 };
 
@@ -209,12 +216,12 @@ function select_queryitem(event){
   var dom_id_regex = /^query-(keyword|history)-([0-9a-f]+)$/;
   var match_result = dom_id_regex.exec(target_dom_id);
   if (match_result === null) {
-    show_error("UI Bug", "Selected DOM id invalid:" + target_dom_id);
+    show_error("UI Bug", "Selected DOM id invalid:" + target_dom_id, 50);
     return;
   }
   var query = shibdata.query_cache[match_result[2]];
   if (! query) {
-    show_error("UI Bug", "Selected query not loaded on browser:" + match_result[2]);
+    show_error("UI Bug", "Selected query not loaded on browser:" + match_result[2], 50);
     return;
   }
   
@@ -225,57 +232,111 @@ function select_queryitem(event){
 function update_mainview(query){
   shibselectedquery = query;
   $('#queryeditor').text(query.querystring);
-  /*TODO: write!!! */
+  update_editbox(query);
 };
+
+function update_editbox(query, optional_state) {
+  var state = optional_state || (query && query.state) || null;
+  var lastresult = last_result(query);
+
+  switch (state) {
+  case undefined:
+  case null:
+    // 'not executed'
+    show_editbox_buttons(['execute_button']);
+    change_editbox_querystatus_style('not executed');
+    break;
+  case 'running':
+    show_editbox_buttons(['pause_button']);
+    change_editbox_querystatus_style('running');
+    break;
+  case 'executed':
+  case 'done':
+    show_editbox_buttons(['rerun_button', 'display_full_button', 'display_head_button', 'download_tsv_button', 'download_csv_button']);
+    change_editbox_querystatus_style('executed', lastresult);
+    break;
+  case 'error':
+    show_editbox_buttons(['rerun_button']);
+    change_editbox_querystatus_style('error', lastresult);
+    break;
+  case 're-running':
+    show_editbox_buttons(['pause_button', 'display_full_button', 'display_head_button', 'download_tsv_button', 'download_csv_button']);
+    change_editbox_querystatus_style('re-running', lastresult);
+    break;
+  default:
+    show_error('UI Bug', 'unknown query status:' + query.state, 50);
+  }
+}
+
+function show_editbox_buttons(buttons){
+  var allbuttons = [
+    'execute_button', 'pause_button', 'rerun_button', 'display_full_button',
+    'display_head_button', 'download_tsv_button', 'download_csv_button'
+  ];
+  if (! buttons)
+    buttons = [];
+  allbuttons.forEach(function(b){
+    if (buttons.indexOf(b) > -1)
+      $('li#' + b).show();
+    else
+      $('li#' + b).hide();
+  });
+}
+
+function change_editbox_querystatus_style(state, result){
+  var allstates = {
+    'not executed':{classname:'status_not_executed', result:false},
+    'running':{classname:'status_running', result:false},
+    'executed':{classname:'status_executed', result:true},
+    'error':{classname:'status_error', result:true},
+    're-running':{classname:'status_rerunning', result:true}
+  };
+  var allclasses = 'status_not_executed status_running status_executed status_error status_rerunning';
+  if (state === 'done')
+    state = 'executed';
+
+  if (allstates[state]) {
+    $('span#querystatus')
+      .removeClass(allclasses)
+      .addClass((allstates[state]).classname)
+      .text(state);
+
+    if ((allstates[state]).result) {
+      $('span#queryresult').show();
+      if (result.error) {
+        $('span#queryresultlines').text(result.error);
+        $('span#queryresultbytes').text("");
+      }
+      else {
+        $('span#queryresultlines').text(" " + result.lines + " lines, ");
+        $('span#queryresultbytes').text(" " + result.bytes + " bytes");
+      }
+    }
+    else {
+      $('span#queryresult').hide();
+    }
+  }
+}
 
 function shib_test_status_change(next_state) {
   if (next_state == 'not executed'){
-    $('li#execute_button').show(); $('li#pause_button').hide(); $('li#rerun_button').hide(); $('li#display_full_button').hide();
-    $('li#display_head_button').hide(); $('li#download_tsv_button').hide(); $('li#download_csv_button').hide();
-    $('span#querystatus')
-      .removeClass('status_running status_executed status_error status_rerunning')
-      .addClass('status_not_executed')
-      .text('not executed');
-    $('span#queryresult').hide();
+    show_editbox_buttons(['execute_button']);
+    change_editbox_querystatus_style('not executed');
   }
   else if (next_state == 'running'){
-    $('li#execute_button').hide(); $('li#pause_button').show(); $('li#rerun_button').hide(); $('li#display_full_button').hide();
-    $('li#display_head_button').hide(); $('li#download_tsv_button').hide(); $('li#download_csv_button').hide();
-    $('span#querystatus')
-      .removeClass('status_not_executed status_executed status_error status_rerunning')
-      .addClass('status_running')
-      .text('running');
-    $('span#queryresult').hide();
+    show_editbox_buttons(['pause_button']);
+    change_editbox_querystatus_style('running');
   }
   else if (next_state == 'executed'){
-    $('li#execute_button').hide(); $('li#pause_button').hide(); $('li#rerun_button').show(); $('li#display_full_button').show();
-    $('li#display_head_button').show(); $('li#download_tsv_button').show(); $('li#download_csv_button').show();
-    $('span#querystatus')
-      .removeClass('status_not_executed status_running status_error status_rerunning')
-      .addClass('status_executed')
-      .text('executed');
-    $('span#queryresult').show();
-    $('span#queryresultlines').text(" 512 lines, ");
-    $('span#queryresultbytes').text(" 2049 bytes");
+    show_editbox_buttons(['rerun_button', 'display_full_button', 'display_head_button', 'download_tsv_button', 'download_csv_button']);
+    change_editbox_querystatus_style('executed', {lines:512, bytes:2049});
   }
   else if (next_state == 'error'){
-    $('li#execute_button').hide(); $('li#pause_button').hide(); $('li#rerun_button').show(); $('li#display_full_button').hide();
-    $('li#display_head_button').hide(); $('li#download_tsv_button').hide(); $('li#download_csv_button').hide();
-    $('span#querystatus')
-      .removeClass('status_not_executed status_running status_executed status_rerunning')
-      .addClass('status_error')
-      .text('error');
-    $('span#queryresult').hide();
+    show_editbox_buttons(['rerun_button']);
+    change_editbox_querystatus_style('error', {error:'hdfs volume full'});
   }
-  else if (next_state == 'rerunning'){
-    $('li#execute_button').hide(); $('li#pause_button').show(); $('li#rerun_button').hide(); $('li#display_full_button').show();
-    $('li#display_head_button').show(); $('li#download_tsv_button').show(); $('li#download_csv_button').show();
-    $('span#querystatus')
-      .removeClass('status_not_executed status_running status_executed status_error')
-      .addClass('status_rerunning')
-      .text('re-running');
-    $('span#queryresult').show();
-    $('span#queryresultlines').text(" 512 lines, ");
-    $('span#queryresultbytes').text(" 2049 bytes");
+  else if (next_state == 're-running'){
+    show_editbox_buttons(['pause_button', 'display_full_button', 'display_head_button', 'download_tsv_button', 'download_csv_button']);
+    change_editbox_querystatus_style('re-running', {lines:1, bytes:50});
   }
 }
