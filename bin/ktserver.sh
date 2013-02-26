9c161051aa217965fb115a69518d7c8dcf725383
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-02-26 18:43:44 +0900

    remove KT related scripts/settings

--- bin/ktserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATADIR=$(dirname $0)/../data
-if [ ! -d $DATADIR ]; then
-    mkdir $DATADIR
-fi
-cd $DATADIR
-exec ktserver shib.kch result.kcd
--- bin/shib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APPDIR=$(dirname $0)/..
-LIBDIR=$APPDIR/lib
-cd $APPDIR
-export NODE_PATH=$LIBDIR:$NODE_PATH
-export NODE_ENV=production
-exec node app.js
