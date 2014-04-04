128f6f9e1d90c4250b06671b33e95973fc552de5
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-04-04 17:52:18 +0900

    add module for db/table access control

---
+++ test/t_access_control.js
@@ -0,0 +1,75 @@
+var testCase = require('nodeunit').testCase;
+var access_control = require('shib/access_control'),
+    AccessControl = access_control.AccessControl;
+
+module.exports = testCase({
+  /*
+  setUp: function (callback) {
+    this.foo = 'bar';
+    callback();
+  },
+   */
+  test1: function (test) {
+    test.equals('bar', 'bar');
+    test.done();
+  },
+  default_rule: function(test){
+    var rule = {};
+    var acl = new AccessControl(rule);
+
+    test.ok(acl.allowed('t1', 'default'));
+    test.ok(acl.allowed('t2', 'default'));
+    test.ok(acl.allowed('WhatsTable', 'db1'));
+
+    test.done();
+  },
+  default_allowed: function(test){
+    var rule = {
+      default: "allow",
+      databases: {
+        secret: { default: "deny" }
+      }
+    };
+    var acl = new AccessControl(rule);
+
+    test.ok(acl.allowed('t1', 'default'));
+    test.ok(acl.allowed('t2', 'default'));
+    test.ok(acl.allowed('WhatsTable', 'db1'));
+
+    test.ok(! acl.allowed('table', 'secret'));
+
+    test.done();
+  },
+  default_denied: function(test){
+    var rule = {
+      default: "deny",
+      databases: {
+        default: { default: "allow" },
+        test: { default: "allow", deny: ["IDMaster", "secretTest"] },
+        data: { default: "deny", allow: ["t1", "t2"] }
+      }
+    };
+    var acl = new AccessControl(rule);
+
+    test.ok(acl.allowed('t1', 'default'));
+    test.ok(acl.allowed('t2', 'default'));
+
+    test.ok(! acl.allowed('WhatsTable', 'db1'));
+
+    test.ok(acl.allowed('table', 'test'));
+    test.ok(acl.allowed('t1', 'test'));
+    test.ok(! acl.allowed('IDMaster', 'test'));
+    test.ok(! acl.allowed('secretTest', 'test'));
+
+    test.ok(! acl.allowed('table', 'data'));
+    test.ok(! acl.allowed('table2', 'data'));
+    test.ok(acl.allowed('t1', 'data'));
+    test.ok(acl.allowed('t2', 'data'));
+
+    test.done();
+  },
+  tearDown: function (callback) {
+    // clean up
+    callback();
+  }
+});
